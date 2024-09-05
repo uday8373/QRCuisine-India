126e20660a7846cd57eee282993a13ba4c6d8116
devmithun89 <mithungorai662@gmail.com>
2024-09-05 22:03:16 +0530

    Order Preview design complete

--- components/modal/Order-Preview.jsx
+++ components/modal/Order-Preview.jsx
@@ -96,6 +96,7 @@ export default function OrderPreview({ isOpen, onOpenChange, orderData }) {
                 </div>
               </ModalFooter>
             </>
+            // ,,,,?
           )}
         </ModalContent>
       </Modal>
