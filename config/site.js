91c4a1864199cb30df1540b9d181ee7be0c66517
devmithun89 <mithungorai662@gmail.com>
2024-09-12 12:51:15 +0530

    minor changes

--- config/site.js
+++ config/site.js
@@ -4,21 +4,11 @@ export const siteConfig = {
     "Book your restaurant table effortlessly with QRCuisine. Discover available tables, make instant reservations, and enjoy a hassle-free dining experience. Perfect for both diners and restaurant owners.",
   navItems: [
     {
-      label: "Home",
-      href: "/",
-    },
-
-    {
       href: "Pricing",
       label: "Pricing",
     },
   ],
   navMenuItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-
     {
       href: "Pricing",
       label: "Pricing",
