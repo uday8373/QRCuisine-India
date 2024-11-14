1aa41f67aa3f8250a35e07020df92c9a10c73a91
shadab15github <shadab150102@gmail.com>
2024-11-14 12:02:43 +0530

    Latest USA code

---
+++ constant/data.js
@@ -0,0 +1,23 @@
+import { Clock, Dinner, Mobile } from "@/public/assets/svg/Home-Page/Index";
+import { AlarmClockCheck, HandPlatter, UserCheck } from "lucide-react";
+
+export const HelpsRestaurantsData = [
+  {
+    title: "Staff Shortages we address that",
+    description:
+      "Provide a seamless ordering experience where customers scan a QR code to order directly from their table, minimizing staff.",
+    icon: HandPlatter,
+  },
+  {
+    title: "Personalized Experience for customers",
+    description:
+      "The system remembers customers' preferences, offering personalized recommendations for a better dining experience.",
+    icon: UserCheck,
+  },
+  {
+    title: "Detailed & Real-time Analytics for Restaurants",
+    description:
+      "QRcuisine provides restaurants real-time data of operations, It will make all transactions transparent to owner and manager.",
+    icon: AlarmClockCheck,
+  },
+];
