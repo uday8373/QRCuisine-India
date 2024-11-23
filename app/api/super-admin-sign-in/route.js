5f582177a47dbd118ff6227a19b884d43e02d90d
devmithun89 <mithungorai662@gmail.com>
2024-11-23 16:31:47 +0530

    create super admin sing in api

---
+++ app/api/super-admin-sign-in/route.js
@@ -0,0 +1,68 @@
+import supabase from "@/config/supabase";
+import { NextResponse } from "next/server";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(request) {
+  try {
+    const headers = {
+      ...corsHeaders,
+    };
+
+    const { email, password } = await request.json();
+
+    // Sign in using Supabase
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return NextResponse.json(
+        { error: error.message, code: "AUTH_FAILED" },
+        { status: 401, headers }
+      );
+    }
+
+    const user = data?.user;
+
+    if (!user?.user_metadata?.isVerified) {
+      return NextResponse.json(
+        {
+          error: "User is unverified",
+          code: "USER_UNVERIFIED",
+        },
+        { status: 403, headers }
+      );
+    }
+
+    if (!user?.user_metadata?.is_super_admin) {
+      return NextResponse.json(
+        {
+          error: "You do not have the necessary permissions",
+          code: "ACCESS_DENIED",
+        },
+        { status: 403, headers }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Super admin authenticated", data },
+      { status: 200, headers }
+    );
+  } catch (err) {
+    console.error("Error authenticating user:", err);
+    return NextResponse.json(
+      { error: "An error occurred", code: "INTERNAL_ERROR" },
+      { status: 500, headers: corsHeaders }
+    );
+  }
+}
